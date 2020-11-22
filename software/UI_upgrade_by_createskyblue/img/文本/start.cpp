7617e6c6e80b3cf63d93a975d84a46ab4332979e
createskyblue <createskyblue@outlook.com>
2020-11-22 15:01:48 +0800

    FixBitmapsTF

--- software/UI_upgrade_by_createskyblue/img/文本/start.cpp
+++ software/UI_upgrade_by_createskyblue/img/文本/start.cpp
@@ -6,19 +6,31 @@ using namespace cv;
 int main(int argc, char** argv) {
     char default_prefix[] = "image";
     char* prefix = default_prefix;
-    if (argc >= 3)
-        prefix = argv[2];
+    char* SetcolorAdd;
+    bool color = 1;
 
-    Mat img = imread(argv[2]);
+    Mat img = imread(argv[1]);
+
+    prefix = argv[2];
+    SetcolorAdd = argv[3];
+
+    if (*SetcolorAdd == 'f')
+        color = 0;
+
     Mat channels[3];
     split(img, channels);
+
     // cv::imshow("image", channels[0]);
     printf("const PROGMEM uint8_t %s[] = {", prefix);
 
-    int Abyte, Check, count = 0, countI = 1;
+    int Abyte = 0, Check, count = 0, countI = 1;
     for (int i = 0; i < channels[0].cols * channels[0].rows; i++) {
-        if (!channels[0].at<uchar>(i))
+        if (channels[0].at<uchar>(i)) {
+            if (color)
+                Abyte |= 1;
+        } else if (!color)
             Abyte |= 1;
+
         if (count == 7) {
             if (countI % 4 == 0) {
                 Abyte >>= 4;
